35129878085805e809f729e2c27a8a0c49f2cfbb
fengzeng <1041935843@qq.com>
2022-09-26 18:24:45 +0800

    测试 dev branch

---
+++ src/main/java/com/xiaofengstu/mpg/Main.java
@@ -0,0 +1,12 @@
+package com.xiaofengstu.mpg;
+
+/**
+ * @Description TODO
+ * @Author fengzeng
+ * @Date 2022/9/26 0026 18:24
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("测试 dev branch");
+  }
+}
